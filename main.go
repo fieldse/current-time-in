1835e689c12034ce981095e8622f977e5f90a534
Matt <mattfields+dev@pm.me>
2024-04-12 22:32:34 +1000

    refactor to expect possible multiple results

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 
 import "errors"
 
-func tzCodeFor(s string) (string, error) {
-	return "", errors.New("not implemented")
+// tzCodeFor returns a full timezone name for the given city/country name.
+// This function fuzzy-matches based on the city name, and attempts to return intelligently
+// from cities matching the name.
+// eg: "London", "London UK", "London United Kingdom" should all return the same result.
+// This will return a slice of results for cities which exist in multiple countries.
+func tzCodeFor(s string) ([]string, error) {
+	return []string{""}, errors.New("not implemented")
 }
--- main_test.go
+++ main_test.go
@@ -10,37 +10,39 @@ import (
 func TestCityCode(t *testing.T) {
 
 	testCases := []struct {
-		name     string
-		expected string
+		name        string
+		expected    string
+		numExpected int
 	}{
-		{"New York", "America/New_York"},         //  split word city
-		{"New York city", "America/New_York"},    //  split word city plus "City" suffix
-		{"philadephia", "America/New_York"},      //  lower case
-		{"Washington", "America/New_York"},       //  city with ambiguous state equivalent
-		{"Washington, D.C.", "America/New_York"}, //  city with abbreviation suffix
-		{"San Francisco", "America/Los_Angeles"}, //  split word city
-		{"Dallas TX", "America/Chicago"},         //  city with state abbreviation -- note this one may be multiple TZ codes
-		{"Dallas, Texas", "America/Chicago"},     //  city with state name -- note this one may be multiple TZ codes
-		{"Chicago", "America/Chicago"},
-		{"Brisbane", "Australia/Brisbane"},    // international city with no country name
-		{"Paris", "Europe/Paris"},             // international city with no country name
-		{"Paris, France", "Europe/Paris"},     // internationa city/country with comma
-		{"Frankfurt", "Europe/Berlin"},        // international city with no country name
-		{"Tokyo", "Asia/Tokyo"},               // international city with no country name
-		{"Birmingham", ""},                    // international city with ambiguous country -- this could be US or UK
-		{"Birmingham UK", "Europe/London"},    // international city with ambiguous country -- should be UK
-		{"Birmingham USA", "America/Chicago"}, // international city with ambiguous country -- should be US CST
-		{"London", "Europe/London"},           // international city with no country name
-		{"London England", "Europe/London"},   // international city/country with no comma
-		{"London UK", "Europe/London"},        // international city/country with alternative country name
-		{"Cairo", "Africa/Cairo"},             // international city with multiple countries, but an obvious default
-		{"Cairo, Egypt", "Africa/Cairo"},
-		{"Beijing", "Asia/Shanghai"},
+		{"New York", "America/New_York", 1},         //  split word city
+		{"New York city", "America/New_York", 1},    //  split word city plus "City" suffix
+		{"philadephia", "America/New_York", 1},      //  lower case
+		{"Washington", "America/New_York", 1},       //  city with ambiguous state equivalent
+		{"Washington, D.C.", "America/New_York", 1}, //  city with abbreviation suffix
+		{"San Francisco", "America/Los_Angeles", 1}, //  split word city
+		{"Dallas TX", "America/Chicago", 1},         //  city with state abbreviation -- note this one may be multiple TZ codes
+		{"Dallas, Texas", "America/Chicago", 1},     //  city with state name -- note this one may be multiple TZ codes
+		{"Chicago", "America/Chicago", 1},
+		{"Brisbane", "Australia/Brisbane", 1},    // international city with no country name
+		{"Paris", "Europe/Paris", 1},             // international city with no country name
+		{"Paris, France", "Europe/Paris", 1},     // internationa city/country with comma
+		{"Frankfurt", "Europe/Berlin", 1},        // international city with no country name
+		{"Tokyo", "Asia/Tokyo", 1},               // international city with no country name
+		{"Birmingham", "", 2},                    // international city with ambiguous country -- this could be US or UK
+		{"Birmingham UK", "Europe/London", 1},    // international city with ambiguous country, with country code -- should be UK
+		{"Birmingham USA", "America/Chicago", 1}, // international city with ambiguous country , with country code -- should be US CST
+		{"London", "Europe/London", 1},           // international city with no country name
+		{"London England", "Europe/London", 1},   // international city/country with no comma
+		{"London UK", "Europe/London", 1},        // international city/country with alternative country name
+		{"Cairo", "Africa/Cairo", 1},             // international city with multiple countries, but an obvious default
+		{"Cairo, Egypt", "Africa/Cairo", 1},
+		{"Beijing", "Asia/Shanghai", 1},
 	}
 
 	for _, tc := range testCases {
 		res, err := tzCodeFor(tc.name)
 		assert.Nilf(t, err, "should not return error")
 		assert.Equalf(t, tc.expected, res, "expected %s to be %s", tc.expected, res)
+		assert.Equalf(t, tc.numExpected, len(res), "expected %d results, got %d", tc.numExpected, len(res))
 	}
 }
