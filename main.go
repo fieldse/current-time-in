2e07cb9b652044dc16e194fefa9f24f8b1f7a6ba
Matt <mattfields+dev@pm.me>
2024-04-12 14:15:00 +1000

    add tests for city names

--- main.go
+++ main.go
@@ -1,2 +1,8 @@
 // Get the current time in a given city
 package main
+
+import "errors"
+
+func tzCodeFor(s string) (string, error) {
+	return "", errors.New("not implemented")
+}
--- main_test.go
+++ main_test.go
@@ -13,32 +13,34 @@ func TestCityCode(t *testing.T) {
 		name     string
 		expected string
 	}{
-		{"New York", ""},      // test split word city
-		{"New York city", ""}, // test split word city plus "City" suffix
-		{"philadephia", ""},   // test lower case
-		{"Washington", ""},
-		{"Washington, D.C.", ""}, // Test city with abbreviation suffix
-		{"San Francisco", ""},    // test split word city
-		{"Dallas TX", ""},        // Test with state abbreviation
-		{"Dallas, Texas", ""},
-		{"brisbane", ""},
+		{"New York", "America/New_York"},         //  split word city
+		{"New York city", "America/New_York"},    //  split word city plus "City" suffix
+		{"philadephia", "America/New_York"},      //  lower case
+		{"Washington", "America/New_York"},       //  city with ambiguous state equivalent
+		{"Washington, D.C.", "America/New_York"}, //  city with abbreviation suffix
+		{"San Francisco", "America/Los_Angeles"}, //  split word city
+		{"Dallas TX", "America/Chicago"},         //  city with state abbreviation -- note this one may be multiple TZ codes
+		{"Dallas, Texas", "America/Chicago"},     //  city with state name -- note this one may be multiple TZ codes
 		{"Chicago", "America/Chicago"},
-		{"paris", ""},
-		{"Frankfurt", ""},
-		{"Tokyo", ""},
-		{"Paris, France", ""},  // test city/country with comma
-		{"Birmingham", ""},     // test city with ambiguous country -- this could be US or UK
-		{"Birmingham UK", ""},  // test city with ambiguous country -- should be UK
-		{"Birmingham USA", ""}, // test city with ambiguous country -- should be US
-		{"London", ""},
-		{"London England", ""}, // test city/country with no comma
-		{"London UK", ""},      // test city/country with alternative country name
-		{"Cairo", ""},
-		{"Cairo, Egypt", ""},
-		{"Beijing", ""},
+		{"Brisbane", "Australia/Brisbane"},    // international city with no country name
+		{"Paris", "Europe/Paris"},             // international city with no country name
+		{"Paris, France", "Europe/Paris"},     // internationa city/country with comma
+		{"Frankfurt", "Europe/Berlin"},        // international city with no country name
+		{"Tokyo", "Asia/Tokyo"},               // international city with no country name
+		{"Birmingham", ""},                    // international city with ambiguous country -- this could be US or UK
+		{"Birmingham UK", "Europe/London"},    // international city with ambiguous country -- should be UK
+		{"Birmingham USA", "America/Chicago"}, // international city with ambiguous country -- should be US CST
+		{"London", "Europe/London"},           // international city with no country name
+		{"London England", "Europe/London"},   // international city/country with no comma
+		{"London UK", "Europe/London"},        // international city/country with alternative country name
+		{"Cairo", "Africa/Cairo"},             // international city with multiple countries, but an obvious default
+		{"Cairo, Egypt", "Africa/Cairo"},
+		{"Beijing", "Asia/Shanghai"},
 	}
 
 	for _, tc := range testCases {
-		assert.Equalf(t, tc.expected, tc.name, "expected %s to be %s", tc.expected, tc.name)
+		res, err := tzCodeFor(tc.name)
+		assert.Nilf(t, err, "should not return error")
+		assert.Equalf(t, tc.expected, res, "expected %s to be %s", tc.expected, res)
 	}
 }
