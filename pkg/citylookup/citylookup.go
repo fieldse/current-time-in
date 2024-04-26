17b5fd855bdb327ea4af5b7bbad516de3409d789
Matt <mattfields+dev@pm.me>
2024-04-26 16:32:15 +1000

    break out load-data funcs

--- pkg/citylookup/citylookup.go
+++ pkg/citylookup/citylookup.go
@@ -7,34 +7,10 @@ import (
 	"github.com/fieldse/current-time-in/pkg/logger"
 	"github.com/fieldse/current-time-in/shared"
 
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"path"
 )
 
 var Logger = logger.Logger
-
-var (
-	DataDirectory string
-	DataJSONFile  string
-)
-
-// Set the data directory
-func init() {
-	rootDir := getProjectRootDir()
-	p := path.Join(rootDir, "data")
-	shared.ExistsOrPanic(p) // validate the data directory exists or panic
-	DataDirectory = p
-	DataJSONFile = path.Join(p, "cityMap.json")
-}
-
-// Return path to the project root directory --
-func getProjectRootDir() string {
-	curDir, _ := os.Getwd()
-	return path.Dir(path.Dir(curDir))
-}
 
 // CityData represents a single entry from the cities data table
 type CityData struct {
@@ -48,38 +24,6 @@ type CityData struct {
 	Iso3      string  `json:"iso3"`
 	Province  string  `json:"province"`
 	Timezone  string  `json:"timezone"`
-}
-
-// tzCodeFor returns a full timezone name for cities matching the given city/country name.
-// This function fuzzy-matches based on the city name, and attempts to return intelligently
-// from cities matching the name.
-// eg: "London", "London UK", "London United Kingdom" should all return the same result.
-// This will return a slice of results for cities which exist in multiple countries.
-func tzCodeFor(_ string) ([]string, error) {
-	return []string{""}, shared.ErrorNotImplementedError{}
-}
-
-// readCityData reads the raw data from cityMap.json to bytes
-func readCityData() ([]byte, error) {
-	b, err := os.ReadFile(DataJSONFile)
-	if err != nil {
-		log.Printf("failed to read cityMap.json: %v", err)
-	}
-	return b, err
-}
-
-// loadCityData loads the city data from cityMap.json
-func loadCityData() ([]CityData, error) {
-	var cityData []CityData
-	b, err := readCityData()
-	if err != nil {
-		return []CityData{}, fmt.Errorf("failed to read city data: %w", err)
-	}
-	err = json.Unmarshal(b, &cityData)
-	if err != nil {
-		return []CityData{}, fmt.Errorf("error parsing city data: %w", err)
-	}
-	return cityData, nil
 }
 
 // findCityExact finds a single city matching exactly by name
@@ -104,4 +48,14 @@ func filterByCountry(rows []CityData, countryName string) []CityData {
 		}
 	}
 	return filtered
+}
+
+// filterByTZCode filters the cities data by fuzzing matching against the given city/country
+// name combination
+// If an exact match by city name is found, it returns immediately.
+// If no match is found by exact city name, it will try to match based on fuzzy search
+// against country name and city/country combination
+// eg: "London", "London UK", "London United Kingdom" should all return the same result.
+func filterByTZCode(rows []CityData, s string) ([]string, error) {
+	return []string{""}, shared.ErrorNotImplementedError{}
 }
